4b4e4d837de561e5610acd3fc04edbe96fb8cc86
Jon Austin <jon.i.austin@gmail.com>
2012-08-13 21:40:12 -0700

    wip

--- Guardfile
+++ Guardfile
@@ -1,14 +1,8 @@
 # A sample Guardfile
 # More info at https://github.com/guard/guard#readme
 
-guard 'rspec', :version => 2 do
+guard 'rspec', :version => 2, :all_after_pass => false, :all_on_start => false, :cli => "--color --format nested --fail-fast" do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
-end
-
-
-guard 'ctags-bundler', :src_path => ["lib", "spec/support"] do
-  watch(/^(lib|spec\/support)\/.*\.rb$/)
-  watch('Gemfile.lock')
 end
--- lib/toptracks.rb
+++ lib/toptracks.rb
@@ -8,12 +8,21 @@ require 'mp3info'
 require 'rainbow'
 require 'ansi/logger'
 
-require 'toptracks/models'
-require 'toptracks/lastfm'
-require 'toptracks/search'
 
 module Toptracks
-  DEBUG=0
-  ECHO=0
-  $logger = ANSI::Logger.new(STDOUT)
+  class Base
+    def initialize
+      @logger = ANSI::Logger.new(STDOUT)
+      @rockstar = Rockstar.lastfm = YAML.load_file(File.join(File.dirname(__FILE__), '../../config/lastfm.yml'))
+      @m3u = File.new(File.join(File.dirname(__FILE__), '../../m3us/_LASTFM-TOP_-_' + @artist.name + '.m3u', 'w'))
+    end
+  end
+
+  class Runner < Base
+    def initialize
+      super
+    end
+  end
 end
+require 'toptracks/models'
+require 'toptracks/search'
