5470d411407b5bb88eb41434c2c23c0204432633
Jon Austin <jon.i.austin@gmail.com>
2013-03-16 13:30:52 -0700

    assist user with initial lastfm api key/secret setup

--- lib/lastfm_m3u/lastfm.rb
+++ lib/lastfm_m3u/lastfm.rb
@@ -1,13 +1,26 @@
+require 'fileutils'
+
 module LastfmM3u
   class Lastfm < Base
     attr_accessor :artist, :root_dir, :limit, :tracks
 
     def initialize(artist, root_dir='/home/jon/music/trance')
-      Rockstar.lastfm = YAML.load_file(File.join(File.dirname(__FILE__), '../../config/lastfm.yml'))
-      @artist = Rockstar::Artist.new artist
-      @root_dir = root_dir
-      @limit = nil
-      super()
+      FileUtils.mkdir_p "#{ENV['HOME']}/.config"
+      config_path = "#{ENV['HOME']}/.config/lastfm_m3u.yml"
+      unless File.exists? config_path
+        FileUtils.cp File.join(File.dirname(__FILE__), '../../config/lastfm.yml.dist'), config_path
+      end
+      config_hash = YAML.load_file(config_path)
+      Rockstar.lastfm = config_hash
+      unless config_hash.values.any? {|v| v==''}
+        @artist = Rockstar::Artist.new artist
+        @root_dir = root_dir
+        @limit = nil
+        super()
+      else
+        $logger.error "\nPlease add your api_key and api_secret to:\n  #{config_path}\nGet your keys at http://www.last.fm/api/account"
+        exit
+      end
     end
 
     def find_tracks(tracks, search_type = :file, progressbar = nil)
--- spec/lastfm_spec.rb
+++ spec/lastfm_spec.rb
@@ -3,6 +3,7 @@ require 'ostruct'
 
 describe LastfmM3u::Lastfm do
   it "should return expected results" do
+    YAML.stub(:load_file).and_return({api_key: 'key', api_secret: 'secret'})
     track = OpenStruct.new(name: 'Poa Alpina')
     lf = LastfmM3u::Lastfm.new('Biosphere', music_dir)
     found = lf.find_tracks([track])
