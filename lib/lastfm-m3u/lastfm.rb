707a79dc47fed1eeb813e4eab9247851ba083766
Jon Austin <jon.i.austin@gmail.com>
2014-01-01 11:49:06 -0800

    refactor base/binary

--- lib/lastfm-m3u/lastfm.rb
+++ lib/lastfm-m3u/lastfm.rb
@@ -1,42 +1,31 @@
 require 'fileutils'
+require 'ruby-progressbar'
 
 module LastfmM3u
   class Lastfm < Base
     attr_accessor :artist, :root_dir, :limit, :tracks
 
-    def initialize(artist, root_dir='/home/jon/music/trance')
+    def initialize(artist, options=OpenStruct.new)
       init_config
       set_artist(artist)
       set_top_tracks
-      @root_dir = root_dir
-      @limit = nil
+      @root_dir = options.directory
+      @limit = options.limit
+      @search_type = options.type || :file
       super()
     end
 
     def set_artist(artist)
-      @artist = Rockstar::Artist.new artist
+      @artist = Rockstar::Artist.new(artist)
     end
 
     def set_top_tracks
       @tracks = @artist.top_tracks
     end
 
-    def find_tracks(search_type = :file, progressbar = nil)
-      begin
-        raise InvalidSearchType unless SEARCH_TYPES.include?(search_type)
-      rescue InvalidSearchType
-        $logger.error("Invalid Search Type '#{search_type}', use one of #{SEARCH_TYPES.join(',')}")
-        exit
-      end
-      found_tracks = {}
-      search = LastfmM3u::FileSearch.new(root_dir)
-      self.limit ||= tracks.size
-      tracks[0..limit-1].each do |track|
-        track.name = CGI.unescapeHTML(track.name)
-        found_tracks[track.name] = search.find(track.name, {search_type: search_type})
-        progressbar.increment if progressbar
-      end
-      found_tracks
+    def find_tracks(search_type=@search_type, progressbar=false)
+      check_for_invalid_search_type(search_type)
+      found_tracks(tracks, search_type, progressbar)
     end
 
 
@@ -69,6 +58,39 @@ module LastfmM3u
     def invalid_config?(config_hash)
       config_hash.values.any? {|v| v==''}
     end
+
+    def search(search_type, track)
+      case search_type
+      when :file, :id3, :both
+        LastfmM3u::FileSearch.new(root_dir).find(track.name, {search_type: search_type})
+      end
+    end
+
+    def found_tracks(tracks, search_type, show_progressbar)
+      self.limit ||= tracks.size
+      progressbar = progressbar
+      tracks[0..limit-1].each_with_object({}) do |track, found_tracks|
+        track.name = CGI.unescapeHTML(track.name)
+        found_tracks[track.name] = search(search_type, track)
+        progressbar.increment if show_progressbar
+      end
+    end
+
+    def check_for_invalid_search_type(search_type=@search_type)
+      begin
+        raise InvalidSearchType unless SEARCH_TYPES.include?(search_type)
+      rescue InvalidSearchType
+        $logger.error("Invalid Search Type '#{search_type}', use one of #{SEARCH_TYPES.join(',')}")
+        exit
+      end
+    end
+  end
+
+  def progressbar
+    ProgressBar.create(
+      starting_at: 0,
+      total: num_tracks,
+      format: "%a".color(:cyan) + "|".color(:magenta) + "%B".color(:green) + "|".color(:magenta) +  "%p%%".color(:cyan))
   end
 
   class InvalidSearchType < Exception; end
--- spec/lastfm_spec.rb
+++ spec/lastfm_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 require 'ostruct'
 
 describe LastfmM3u::Lastfm do
+  let(:options) { OpenStruct.new(directory: music_dir, search_type: :file) }
+
   it "should successfully run full circle" do
     YAML.stub(:load_file).and_return({api_key: 'key', api_secret: 'secret'})
     track = OpenStruct.new(name: 'Poa Alpina')
-    lf = LastfmM3u::Lastfm.new('Biosphere', music_dir)
+    lf = LastfmM3u::Lastfm.new('Biosphere', options)
     lf.send(:tracks=, [track])
     found = lf.find_tracks
     found.should == { track.name => ["#{music_dir}/biosphere/poa-alpina.mp3"] }
